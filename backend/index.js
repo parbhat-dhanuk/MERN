8785acd1f09d035b9386d608b23e64b8001df54e
parbhat-dhanuk <parbhatdhanuk3@gmail.com>
2024-12-20 21:54:32 +0545

    cors setup completed

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
 import express from "express"
 import { config } from "dotenv"
 import connectDB from "./database/connection.js"
+import cookieParser from "cookie-parser"
+import cors from "cors"
 config()
 const app=express()
 const PORT=process.env.PORT
 
+app.use(express.json())  // to parse the json file
+app.use(cookieParser())  // to verify the token from cookie
+
+
+//cors
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true                         
+  }))
 
 app.listen(PORT,()=>{
     connectDB()
