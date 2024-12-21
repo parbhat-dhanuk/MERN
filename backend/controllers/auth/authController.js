98d6cf9ea9050448ad92630a3454a30541b653f3
parbhat-dhanuk <parbhatdhanuk3@gmail.com>
2024-12-21 13:23:53 +0545

    login & logout completed backend

--- backend/controllers/auth/authController.js
+++ backend/controllers/auth/authController.js
@@ -1,5 +1,6 @@
 import User from '../../models/userModel.js'
 import bcrypt from 'bcryptjs'
+import jwt from "jsonwebtoken"
 
 export const register = async (req, res) => {
   try {
@@ -36,6 +37,51 @@ export const register = async (req, res) => {
       .json({ message: 'user registered successfully', data: newUser })
   } catch (error) {
     console.log('error in register controller', error.message)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+}
+
+
+export const login =async(req,res)=>{
+  try {
+    const {email,password}=req.body
+    if(!email||!password){
+      return res.status(400).json({message:"Please provide email and password"})
+    }
+    const cleanedEmail = email.replace(/\s+/g, '')
+    const cleanedPassword = password.replace(/\s+/g, '')
+
+   const checkUser=await User.findOne({email:cleanedEmail})
+   if(!checkUser){
+    return res.status(400).json({message:"Invalid email"})
+   }
+
+   const checkPassword=await bcrypt.compare(cleanedPassword,checkUser.password)
+   if(!checkPassword){
+    return res.status(400).json({message:"Invalid password"})
+   }
+
+   const token=jwt.sign({id:checkUser._id},process.env.JWT_SECRET,{expiresIn:"1d"})
+
+   return res.status(200).cookie('token',token,{httpOnly:true,sameSite:"strict",maxAge:1 * 24 * 60 * 60 * 1000}).json({
+    message:"Loggedin successfully",
+    token:token
+})
+    
+  } catch (error) {
+    console.log('error in login controller', error.message)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+}
+
+
+export const logout=async(_,res)=>{
+  try {
+    return res.cookie('token','',{maxAge:0}).status(200).json({
+      message:"Logged out successfully"
+  })
+  } catch (error) {
+    console.log('error in logout controller', error.message)
     res.status(500).json({ error: 'Internal server error' })
   }
 }
--- backend/routes/auth/authRoute.js
+++ backend/routes/auth/authRoute.js
@@ -1,7 +1,9 @@
 import express from "express"
-import { register } from "../../controllers/auth/authController.js"
+import { login, logout, register } from "../../controllers/auth/authController.js"
 const router=express.Router()
 
 router.route('/register').post(register)
+router.route('/login').post(login)
+router.route('/logout').post(logout)
 
 export default router
